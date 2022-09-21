5d26d90e7e3e9c136a7447cd45daf16bf8e48e25
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2022-09-21 08:18:13 +0300

    Support search and change format

--- index.js
+++ index.js
@@ -1,62 +1,113 @@
-const fetch = require("node-fetch");
-const express = require('express');
-var cors = require('cors');
+const axios = require("axios").default;
+const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+const snap = axios.create({
+  baseURL: "https://api.snapcraft.io",
+  headers: {
+    "X-Ubuntu-Series": "16", // for v1
+    "Snap-Device-Series": "16", // for v2
+  },
+});
 app.use(
-    cors({
-        credentials: true,
-        origin: true
-    })
+  cors({
+    credentials: true,
+    origin: true,
+  })
 );
 
-app.options('*', cors());
+app.options("*", cors());
 
-app.get('/' , (req, res) => {
-  res.send({title : "It is run"});
-})
+app.get("/", (req, res) => {
+  res.send({ status: true });
+});
 
-app.get('/api/:name', async (req, res) => {
-  let snap = await getSnap(req.params.name);
-  res.send(softSnap(snap));
-})
+app.get("/snap/download/:name", async (req, res) => {
+  try {
+    let { data: info } = await snap.get("/v2/snaps/info/" + req.params.name);
+    let { data: details } = await snap.get(
+      "/api/v1/snaps/details/" + req.params.name
+    );
+
+    //
+    res.send({
+      details: {
+        title: details.title,
+        icon: details.icon_url,
+        publisher: details.publisher,
+        name: req.params.name,
+      },
+      download: downloadSize(info),
+    });
+  } catch (e) {
+    res.sendStatus(404);
+  }
+});
+
+app.get("/snap/search/:name", async (req, res) => {
+  try {
+    const result = await snap.get("/v2/snaps/find?q=" + req.params.name);
+    // Get fist 10 result
+    const snapsList = result.data.results.slice(0, 10);
+
+    // Get icon for all snaps
+    const snapsPromise = snapsList.map(async ({ name }) => {
+      const { data: details } = await snap.get("/api/v1/snaps/details/" + name);
+
+      return {
+        title: details.title,
+        name,
+        icon: details.icon_url,
+      };
+    });
+
+    //
+    res.send(await Promise.all(snapsPromise));
+  } catch (e) {
+    res.sendStatus(404);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
-async function getSnap(name) {
-  let snap = await fetch("http://api.snapcraft.io/v2/snaps/info/" + name, {
-    method: "GET",
-    headers: {
-      "Snap-Device-Series": "16",
-    },
-  });
-  return await snap.json();
-}
-
-function softSnap(responsJson) {
-  let architecture = [...responsJson["channel-map"].map(
+function downloadSize(snapInfo) {
+  // Get support architecture in this snap
+  const architectures = Array.from(snapInfo["channel-map"]).map(
     (e) => e.channel.architecture
-  )];
-  architecture = architecture.filter(
-    (element, index, array) => array.findIndex((e) => e === element) === index
   );
-  let snaps = {};
-  architecture.forEach((e) => (snaps[e] = []));
-  responsJson["channel-map"].forEach((e) => {
-    e.download.size = bytesToSize(e.download.size)
-    snaps[e.channel.architecture].push(e);
+
+  // Create object of architecture
+  // Type: { architecture: { title: string; size: string; url: string; }[] }
+  const snaps = {};
+  architectures.forEach((element) => (snaps[element] = []));
+
+  //
+  snapInfo["channel-map"].forEach((e) => {
+    const snap = {
+      name: "",
+      size: "",
+      url: "",
+    };
+    snap.name = e.channel.name;
+    snap.size = bytesToSize(e.download.size);
+    snap.url = e.download.url;
+
+    // Add snap in result
+    snaps[e.channel.architecture].push(snap);
   });
-  responsJson["channel-map"] = snaps;
-  return responsJson;
+
+  //
+  return snaps;
 }
 
 function bytesToSize(bytes) {
   var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
   if (bytes == 0) return "0 Byte";
   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-  return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
+  return Math.round(bytes / Math.pow(1024, i), 2) + sizes[i];
 }
-
